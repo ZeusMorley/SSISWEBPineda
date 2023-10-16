d8485cfec66b3c4c2b4355fab2cbafb38eddac83
Zeus Morley S. Pineda <20211853@gmail.com>
2023-10-17 04:26:43 +0800

    Finished MainLayout

--- app.py
+++ app.py
@@ -4,5 +4,21 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def app_open():
     return render_template('index.html')
+
+@app.route("/studenttab")
+def studenttab_open():
+    return render_template('studenttab.html')
+
+@app.route("/coursetab")
+def coursetab_open():
+    return render_template('coursetab.html')
+
+@app.route("/collegetab")
+def collegetab_open():
+    return render_template('collegetab.html')
+
+@app.route("/searchtab")
+def searchtab():
+    return render_template('searchtab.html')
