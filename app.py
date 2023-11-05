d95f641206d7305abb6fa827c110b925838799e1
Zeus Morley S. Pineda <20211853@gmail.com>
2023-11-05 18:42:55 +0800

    Added flash in colleges:

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-from flask import Flask
+from flask import Flask, g, render_template
 from db import create_table
-
-app = Flask(__name__)
-
-# Import blueprints
 from views.colleges import colleges_bp
 from views.courses import courses_bp
 from views.students import students_bp
 from views.search import search_bp
+import pymysql
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+
+# MySQL Configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'wordpass'
+app.config['MYSQL_DB'] = 'zeusdb'
+
+def create_mysql_connection():
+    return pymysql.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        db=app.config['MYSQL_DB']
+    )
+
+@app.before_request
+def before_request():
+    g.mysql = create_mysql_connection()
+
+@app.teardown_request
+def teardown_request(exception):
+    mysql = getattr(g, 'mysql', None)
+    if mysql is not None:
+        mysql.close()
 
 # Register blueprints
 app.register_blueprint(colleges_bp)
@@ -15,6 +39,10 @@ app.register_blueprint(courses_bp)
 app.register_blueprint(students_bp)
 app.register_blueprint(search_bp)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
-    create_table()
+    create_table(create_mysql_connection())  # Creating tables at the start
     app.run(debug=True)
--- db.py
+++ db.py
@@ -2,58 +2,47 @@ import sys
 sys.path.insert(0,"C:/laragon/bin/python/python-3.10/Scripts")
 import pymysql
 
-def create_table():
-    app = Flask(__name__)
+def create_table(mysql):
+    try:
+        with mysql.cursor() as cur:
+            cur.execute("""CREATE TABLE IF NOT EXISTS `zeusdb`.`college` (
+                `CollegeCode` VARCHAR(8) NOT NULL,
+                `CollegeName` VARCHAR(32) NOT NULL,
+                PRIMARY KEY (`CollegeCode`),
+                UNIQUE INDEX `CollegeName_UNIQUE` (`CollegeName` ASC) VISIBLE
+            )""")
 
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'wordpass'
-    app.config['MYSQL_DB'] = 'zeusdb'
+            cur.execute("""CREATE TABLE IF NOT EXISTS `zeusdb`.`course` (
+              `CourseCode` VARCHAR(8) NOT NULL,
+              `CourseName` VARCHAR(32) NOT NULL,
+              `CollegeCode` VARCHAR(8) NOT NULL,
+              PRIMARY KEY (`CourseCode`),
+              UNIQUE INDEX `CourseName_UNIQUE` (`CourseName` ASC) VISIBLE,
+              INDEX `CollegeCode_idx` (`CollegeCode` ASC) VISIBLE,
+              CONSTRAINT `CollegeCode`
+                FOREIGN KEY (`CollegeCode`)
+                REFERENCES `zeusdb`.`college` (`CollegeCode`)
+                ON DELETE NO ACTION
+                ON UPDATE NO ACTION
+            )""")
 
-    mysql = pymysql.connect(
-        host=app.config['MYSQL_HOST'],
-        user=app.config['MYSQL_USER'],
-        password=app.config['MYSQL_PASSWORD'],
-        db=app.config['MYSQL_DB']
-    )
+            cur.execute("""CREATE TABLE IF NOT EXISTS `zeusdb`.`student` (
+              `StudentId` VARCHAR(8) NOT NULL,
+              `StudentFirstName` VARCHAR(32) NOT NULL,
+              `StudentLastName` VARCHAR(32) NOT NULL,
+              `CourseCode` VARCHAR(8) NOT NULL,
+              `StudentYearLvl` VARCHAR(4) NOT NULL,
+              `StudentGender` VARCHAR(8) NOT NULL,
+              PRIMARY KEY (`StudentId`),
+              INDEX `CourseCode_idx` (`CourseCode` ASC) VISIBLE,
+              CONSTRAINT `CourseCode`
+                FOREIGN KEY (`CourseCode`)
+                REFERENCES `zeusdb`.`course` (`CourseCode`)
+                ON DELETE NO ACTION
+                ON UPDATE NO ACTION
+            )""")
 
-    cur = mysql.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS `zeusdb`.`college` (
-      `CollegeCode` VARCHAR(8) NOT NULL,
-      `CollegeName` VARCHAR(32) NOT NULL,
-      PRIMARY KEY (`CollegeCode`),
-      UNIQUE INDEX `CollegeName_UNIQUE` (`CollegeName` ASC) VISIBLE
-    )""")
+        mysql.commit()
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS `zeusdb`.`course` (
-      `CourseCode` VARCHAR(8) NOT NULL,
-      `CourseName` VARCHAR(32) NOT NULL,
-      `CollegeCode` VARCHAR(8) NOT NULL,
-      PRIMARY KEY (`CourseCode`),
-      UNIQUE INDEX `CourseName_UNIQUE` (`CourseName` ASC) VISIBLE,
-      INDEX `CollegeCode_idx` (`CollegeCode` ASC) VISIBLE,
-      CONSTRAINT `CollegeCode`
-        FOREIGN KEY (`CollegeCode`)
-        REFERENCES `zeusdb`.`college` (`CollegeCode`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
-    )""")
-
-    cur.execute("""CREATE TABLE IF NOT EXISTS `zeusdb`.`student` (
-      `StudentId` VARCHAR(8) NOT NULL,
-      `StudentFirstName` VARCHAR(32) NOT NULL,
-      `StudentLastName` VARCHAR(32) NOT NULL,
-      `CourseCode` VARCHAR(8) NOT NULL,
-      `StudentYearLvl` VARCHAR(4) NOT NULL,
-      `StudentGender` VARCHAR(8) NOT NULL,
-      PRIMARY KEY (`StudentId`),
-      INDEX `CourseCode_idx` (`CourseCode` ASC) VISIBLE,
-      CONSTRAINT `CourseCode`
-        FOREIGN KEY (`CourseCode`)
-        REFERENCES `zeusdb`.`course` (`CourseCode`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION
-    )""")
-
-    mysql.commit()
-    cur.close()
+    except Exception as e:
+        print("An error occurred:", e)
