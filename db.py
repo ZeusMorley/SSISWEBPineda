10f31d5e1a03f0200ddc9f509f23ba37adcd9fa0
Zeus Morley S. Pineda <20211853@gmail.com>
2023-11-09 04:41:50 +0800

    altered the table

--- db.py
+++ db.py
@@ -7,14 +7,14 @@ def create_table(mysql):
         with mysql.cursor() as cur:
             cur.execute("""CREATE TABLE IF NOT EXISTS `zeusdb`.`college` (
                 `CollegeCode` VARCHAR(8) NOT NULL,
-                `CollegeName` VARCHAR(32) NOT NULL,
+                `CollegeName` VARCHAR(64) NOT NULL,
                 PRIMARY KEY (`CollegeCode`),
                 UNIQUE INDEX `CollegeName_UNIQUE` (`CollegeName` ASC) VISIBLE
             )""")
 
             cur.execute("""CREATE TABLE IF NOT EXISTS `zeusdb`.`course` (
               `CourseCode` VARCHAR(8) NOT NULL,
-              `CourseName` VARCHAR(32) NOT NULL,
+              `CourseName` VARCHAR(64) NOT NULL,
               `CollegeCode` VARCHAR(8) NOT NULL,
               PRIMARY KEY (`CourseCode`),
               UNIQUE INDEX `CourseName_UNIQUE` (`CourseName` ASC) VISIBLE,
@@ -27,9 +27,9 @@ def create_table(mysql):
             )""")
 
             cur.execute("""CREATE TABLE IF NOT EXISTS `zeusdb`.`student` (
-              `StudentId` VARCHAR(8) NOT NULL,
-              `StudentFirstName` VARCHAR(32) NOT NULL,
-              `StudentLastName` VARCHAR(32) NOT NULL,
+              `StudentId` VARCHAR(9) NOT NULL,
+              `StudentFirstName` VARCHAR(64) NOT NULL,
+              `StudentLastName` VARCHAR(64) NOT NULL,
               `CourseCode` VARCHAR(8) NOT NULL,
               `StudentYearLvl` VARCHAR(4) NOT NULL,
               `StudentGender` VARCHAR(8) NOT NULL,
